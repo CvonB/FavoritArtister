2e91ea5301219f9e3cba3dcc3d5f81cf26b9db00
Christian von Bothmer <cvb.jaf@gmail.com>
2022-12-07 14:27:13 +0100

    Last Stuff

--- Acme/Controllers/BandsController.cs
+++ Acme/Controllers/BandsController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FavoritArtister.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FavoritArtister.Controllers
 {
     public class BandsController : Controller
     {
+
+        BandService service;
+        public BandsController() { 
+            service = new BandService();
+        }
+
+        [HttpGet(""), HttpGet("index"), HttpGet("bands")]
         public IActionResult Index()
         {
-            return View();
+            var model = service.GetAllBands();
+            return View(model);
         }
     }
 }
