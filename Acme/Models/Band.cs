0f9093b8f72ab039213402c7cccb2d2a3946c1f2
Christian von Bothmer <cvb.jaf@gmail.com>
2022-12-07 14:14:24 +0100

    Added basics

---
+++ Acme/Models/Band.cs
@@ -0,0 +1,9 @@
+﻿namespace FavoritArtister.Models
+{
+    public class Band
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Acme/Models/BandService.cs
@@ -0,0 +1,24 @@
+﻿namespace FavoritArtister.Models
+{
+    public class BandService
+    {
+        static int id = 1;
+        private List<Band> bandList = new List<Band>()
+        {
+            new Band{ ID = id++, Name = "Pink Floyd", Description = "Mindre coolt rock band" },
+            new Band{ ID = id++, Name = "Rammstein", Description = "Rätt så coolt rock band" },
+            new Band{ ID = id++, Name = "Iron Maiden", Description = "Väldigt coolt rock band" }
+       };
+
+        public Band GetBandByID(int id)
+        {
+            return bandList[id];
+        }
+
+        public Band[] GetAllBands()
+        {
+            return bandList
+                .ToArray();
+        }
+    }
+}
